dba2721fc58371d2cc941771837c0b013d7af29a
Evan Hennessy <hennessyevan@users.noreply.github.com>
2019-02-05 13:58:00 -0500

    add configurable returnValue to timezone component

--- src/timezone/src/TimeZone.tsx
+++ src/timezone/src/TimeZone.tsx
@@ -2,27 +2,54 @@ import * as React from 'react'
 import SelectField from '../../select/src/SelectField'
 import moment from 'moment-timezone'
 import { timezones } from './data/timezones'
+import { SelectProps } from '../../select/src/Select'
 
-export default class TimeZone extends React.Component<any, any> {
+export interface TimeZoneProps extends SelectProps {
+  /**
+   * Timezone Array Items
+   * defaults to aluminum timezones cldr
+   */
+  items?: string[] | object[]
+
+  /**
+   * value to return onChange
+   */
+  returnValue?: 'value' | 'abbr' | 'offset' | 'isdst' | 'text' | 'utc'
+}
+
+export default class TimeZone extends React.Component<TimeZoneProps> {
   items: any = timezones
   guessedZone: any = moment.tz.guess()
+  default: any
+
+  getOptions = (items: object[]) => {
+    if (!items) {
+      return <option>No Options Loaded</option>
+    }
+
+    const { returnValue = 'text' } = this.props
+
+    return items.map((item: any, i: number) => {
+      const selected: boolean = item.utc.includes(this.guessedZone)
+      if (selected) {
+        this.default = selected
+      }
+
+      return (
+        <option
+          key={item.abbr + i}
+          selected={selected ? true : false}
+          value={String(item[returnValue])}
+          data-return-value={String(returnValue)}
+        >
+          {item.text}
+        </option>
+      )
+    })
+  }
 
   render() {
     const { items = this.items } = this.props
-    return (
-      <SelectField {...this.props}>
-        {items &&
-          items.map((item: any) => {
-            const selected: boolean = item.utc.includes(this.guessedZone)
-            console.log(selected)
-
-            return (
-              <option key={item.abbr} selected={selected} value={item}>
-                {item.text}
-              </option>
-            )
-          })}
-      </SelectField>
-    )
+    return <SelectField {...this.props}>{this.getOptions(items)}</SelectField>
   }
 }
--- stories/timezone.stories.tsx
+++ stories/timezone.stories.tsx
@@ -2,9 +2,23 @@ import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import Box from '@hennessyevan/aluminum-box'
 import { TimeZone } from '../src'
+import { Manager } from '../src/manager'
 
 storiesOf('timezone', module).add('TimeZone', () => (
   <Box>
-    <TimeZone />
+    <Manager>
+      {({ state, setState }) => (
+        <React.Fragment>
+          <TimeZone
+            name="test"
+            returnValue="utc"
+            onChange={e =>
+              setState({ [e.target.name]: JSON.stringify(e.target.value) })
+            }
+          />
+          <pre>{JSON.stringify(state, null, 3)}</pre>
+        </React.Fragment>
+      )}
+    </Manager>
   </Box>
 ))
